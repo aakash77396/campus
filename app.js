cbd83f4e906475b3883d6d5e11dd6f19136e62cc
Aakash Kumar <122362592+aakash77396@users.noreply.github.com>
2025-04-17 22:56:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:4000", // ✅ React frontend ka URL
+        origin: "https://aesthetic-truffle-e2a62e.netlify.app", // ✅ React frontend ka URL
         methods: ["GET", "POST"],
     },
 });
